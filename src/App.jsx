3aa2a9d54943a60eb71a02ae7aeb5feb6f1e8cba
RodriMR <rodrimr2008@gmail.com>
2022-06-27 19:05:57 -0300

    Natours first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+function App() {
+  return (
+    <div className="div1">
+      div1
+      <div className="div2"> div2</div>
+      <p>holis soy un p</p>
+    </div>
+  );
+}
+
+export default App;
