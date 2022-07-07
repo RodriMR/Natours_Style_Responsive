d7e29605c997eef40b4286c4a6179d6c876790eb
RodriMR <rodrimr2008@gmail.com>
2022-07-07 19:44:39 -0300

    Features section tours finished

--- Natours/src/App.js
+++ Natours/src/App.js
@@ -80,7 +80,7 @@ function App() {
                 </h3>
                 <p className="feature-box__text ">
                   Qui rerum facere sed ipsum impedit ab natus quod a consequatur
-                  autem.
+                  autem.Et accusantium autem quo nostrum velit.
                 </p>
               </div>
             </div>
@@ -92,7 +92,7 @@ function App() {
                 </h3>
                 <p className="feature-box__text ">
                   Qui rerum facere sed ipsum impedit ab natus quod a consequatur
-                  autem.
+                  autem.Et accusantium autem quo nostrum velit.
                 </p>
               </div>
             </div>
@@ -104,7 +104,7 @@ function App() {
                 </h3>
                 <p className="feature-box__text ">
                   Qui rerum facere sed ipsum impedit ab natus quod a consequatur
-                  autem.
+                  autem.Et accusantium autem quo nostrum velit.
                 </p>
               </div>
             </div>
@@ -116,10 +116,118 @@ function App() {
                 </h3>
                 <p className="feature-box__text ">
                   Qui rerum facere sed ipsum impedit ab natus quod a consequatur
-                  autem.
+                  autem.Et accusantium autem quo nostrum velit.
                 </p>
               </div>
             </div>
+          </div>
+        </section>
+        <section className="section-tours">
+          <div className="u-center-text u-margin-bottom-big">
+            <h2 className="heading-secondary">Most popular tours</h2>
+          </div>
+          <div className="row">
+            <div className="col-1-of-3">
+              <div className="card">
+                <div className="card__side card__side--front card__side--front-1">
+                  <div className="card__picture card__picture--1">&nbsp;</div>
+                  <h4 className="card__heading">
+                    <span className="card__heading-span card__heading-span--1">
+                      The sea explorer
+                    </span>
+                  </h4>
+                  <div className="card__details">
+                    <ul>
+                      <li>3 day tour</li>
+                      <li>Up to 30 people</li>
+                      <li>2 tour guides</li>
+                      <li>Sleep in cozy hotels</li>
+                      <li>Difficulty: easy</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card__side card__side--back card__side--back-1">
+                  <div className="card__cta">
+                    <div className="card__price-box">
+                      <p className="card__price-only">Only</p>
+                      <p className="card__price-value">$297</p>
+                    </div>
+                    <a href="" className="btn btn--white">
+                      Book now!
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-1-of-3">
+              <div className="card">
+                <div className="card__side card__side--front card__side--front-2">
+                  <div className="card__picture card__picture--2">&nbsp;</div>
+                  <h4 className="card__heading">
+                    <span className="card__heading-span card__heading-span--2">
+                      The forest hiker
+                    </span>
+                  </h4>
+                  <div className="card__details">
+                    <ul>
+                      <li>7 day tour</li>
+                      <li>Up to 40 people</li>
+                      <li>6 tour guides</li>
+                      <li>Sleep in provided tents</li>
+                      <li>Difficulty: medium</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card__side card__side--back card__side--back-2">
+                  <div className="card__cta">
+                    <div className="card__price-box">
+                      <p className="card__price-only">Only</p>
+                      <p className="card__price-value">$297</p>
+                    </div>
+                    <a href="" className="btn btn--white">
+                      Book now!
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-1-of-3">
+              <div className="card">
+                <div className="card__side card__side--front card__side--front-3">
+                  <div className="card__picture card__picture--3">&nbsp;</div>
+                  <h4 className="card__heading">
+                    <span className="card__heading-span card__heading-span--3">
+                      The sea explorer
+                    </span>
+                  </h4>
+                  <div className="card__details">
+                    <ul>
+                      <li>3 day tour</li>
+                      <li>Up to 30 people</li>
+                      <li>2 tour guides</li>
+                      <li>Sleep in cozy hotels</li>
+                      <li>Difficulty: easy</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card__side card__side--back card__side--back-3">
+                  <div className="card__cta">
+                    <div className="card__price-box">
+                      <p className="card__price-only">Only</p>
+                      <p className="card__price-value">$297</p>
+                    </div>
+                    <a href="" className="btn btn--white">
+                      Book now!
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="u-center-text u-margin-top-huge">
+            <a href="#" className="btn btn--green">
+              Discover all tours
+            </a>
           </div>
         </section>
       </main>
