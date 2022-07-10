7e4b8768b7c9cfc41f0ba4341eab9fff36c1e0bd
RodriMR <rodrimr2008@gmail.com>
2022-07-09 22:14:21 -0300

    Form done

--- Natours/src/App.js
+++ Natours/src/App.js
@@ -305,6 +305,74 @@ function App() {
             </a>
           </div>
         </section>
+        <section className="section-book">
+          <div className="row">
+            <div className="book">
+              <div className="book__form">
+                <form action="#" className="form">
+                  <div className="u-margin-bottom-medium">
+                    <h2 className="heading-secondary">Start booking now</h2>
+                  </div>
+                  <div className="form__group">
+                    <input
+                      type="text"
+                      className="form__input"
+                      placeholder="Full name"
+                      required
+                      id="name"
+                      autocomplete="off"
+                    />
+                    <label for="name" htmlFor="" className="form__label">
+                      Full name
+                    </label>
+                  </div>
+                  <div className="form__group">
+                    <input
+                      type="email"
+                      className="form__input"
+                      placeholder="Email address"
+                      required
+                      id="email"
+                      autocomplete="off"
+                    />
+                    <label for="email" htmlFor="" className="form__label">
+                      Email adress
+                    </label>
+                  </div>
+                  <div className="form__group u-margin-bottom-medium">
+                    <div className="form__radio-group">
+                      <input
+                        type="radio"
+                        className="form__radio-input"
+                        id="small"
+                        name="size"
+                      />
+                      <label for="small" className="form__radio-label">
+                        <span className="form__radio-button"></span>
+                        Small tour group
+                      </label>
+                    </div>
+                    <div className="form__radio-group">
+                      <input
+                        type="radio"
+                        className="form__radio-input"
+                        id="large"
+                        name="size"
+                      />
+                      <label for="large" className="form__radio-label">
+                        <span className="form__radio-button"></span>
+                        Large tour group
+                      </label>
+                    </div>
+                  </div>
+                  <div className="form__group">
+                    <button className="btn btn--green">Next Step &rarr;</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
       {/* <section className="grid-test">
         <div className="row">
