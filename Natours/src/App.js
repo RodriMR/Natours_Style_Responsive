6dd7cdf384436908c4994f0c6779355ae5bde697
RodriMR <rodrimr2008@gmail.com>
2022-07-10 18:03:31 -0300

    footer done

--- Natours/src/App.js
+++ Natours/src/App.js
@@ -374,6 +374,62 @@ function App() {
           </div>
         </section>
       </main>
+      <footer className="footer">
+        <div className="footer__logo-box">
+          <img
+            src={require("../src/img/logo-green-2x.png")}
+            alt=""
+            className="footer__logo"
+          />
+        </div>
+        <div className="row">
+          <div className="col-1-of-2">
+            <div className="footer__navigation">
+              <ul className="footer__list">
+                <li className="footer__item">
+                  <a href="" className="footer__link">
+                    Company
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a href="" className="footer__link">
+                    Contact us
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a href="" className="footer__link">
+                    Careers
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a href="" className="footer__link">
+                    Privacy policy
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a href="" className="footer__link">
+                    Terms
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-1-of-2">
+            <p className="footer__copyright">
+              Built by{" "}
+              <a href="" className="footer__link">
+                {" "}
+                Rodrigo Molina{" "}
+              </a>{" "}
+              for his online course
+              <a href="" className="footer__link">
+                Advanced CSS and Sass
+              </a>
+              . Copyrigth &copy; by Jonas Schmedtmann
+            </p>
+          </div>
+        </div>
+      </footer>
       {/* <section className="grid-test">
         <div className="row">
           <div className="col-1-of-2">Col 1 of 2</div>
